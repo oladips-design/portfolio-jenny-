0c5c18e2f0c2f412899ecf86da195d81e3bcff20
noobDEV0007 <oladeleoladipupo555@gmail.com>
2024-06-04 00:00:42 +0100

    background image added

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Strengths from "./components/Strengths";
 import Work from "./components/Work";
-
+import bgImg from "./assets/unsplash_ek7Y_iOl3UQ(2).png";
+import "./App.css";
 function App() {
   return (
     <>
       <div className="main">
+        <div className="bg_group">
+          <img src={bgImg} className="bg" />
+          <img src={bgImg} className="bg" />
+          <img src={bgImg} className="bg" />
+        </div>
+
         <Navbar />
         <Hero />
         <Work />
         <About />
-        <Strengths />
-        <Experience />
+        <div className="bg_container">
+          <Strengths />
+          <Experience />
+          <img src={bgImg} className="bg" />
+        </div>
       </div>
     </>
   );
